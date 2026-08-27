90259bef77d203de8f357fb6112e5351cde93b84
Ray <d2arknotfound@gmail.com>
2026-08-27 18:18:30 +0530

    Fix syntax error in site.tsx configuration

--- src/config/site.tsx
+++ src/config/site.tsx
@@ -124,6 +124,8 @@ export const siteConfig: SiteConfigType = {
         // @ts-expect-error: Property 'adult' is missing in type 'TopRatedTvShowResult' but required in type 'TV'.
         query: () => tmdb.tvShows.topRated(),
         param: "topRated",
+      {,
+    ],
   },
 };
 
