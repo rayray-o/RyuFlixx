c68af7751b3b7bd8539d4b41e10debfb9387464a
Ray <d2arknotfound@gmail.com>
2026-08-27 19:24:08 +0530

    Refactor Header component for improved readability

--- src/components/sections/Movie/Player/Header.tsx
+++ src/components/sections/Movie/Player/Header.tsx
@@ -21,17 +21,25 @@ const MoviePlayerHeader: React.FC<MoviePlayerHeaderProps> = ({
       className={cn(
         "absolute top-0 z-40 flex h-28 w-full items-start justify-between gap-4",
         "bg-linear-to-b from-black/80 to-transparent p-2 text-white transition-opacity md:p-4",
-        { "opacity-0": hidden },
+        { "pointer-events-none opacity-0": hidden },
       )}
     >
       <ActionButton label="Back" href={`/movie/${id}`}>
         <ArrowLeft size={42} />
       </ActionButton>
+
       <div className="absolute left-1/2 hidden -translate-x-1/2 flex-col justify-center text-center sm:flex">
-        <p className="text-sm text-white text-shadow-lg sm:text-lg lg:text-xl">{movieName}</p>
+        <p className="text-sm text-white text-shadow-lg sm:text-lg lg:text-xl">
+          {movieName}
+        </p>
       </div>
+
       <div className="flex items-center gap-4">
-        <ActionButton label="Sources" tooltip="Sources" onClick={onOpenSource}>
+        <ActionButton
+          label="Sources"
+          tooltip="Sources"
+          onClick={onOpenSource}
+        >
           <Server size={34} />
         </ActionButton>
       </div>
