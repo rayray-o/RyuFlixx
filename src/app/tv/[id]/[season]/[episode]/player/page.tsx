93c954f1430038ff12615b99896fb5849e25db9b
Ray <d2arknotfound@gmail.com>
2026-09-04 23:44:01 +0530

    Refactor TvShowPlayerPage component for clarity

--- src/app/tv/[id]/[season]/[episode]/player/page.tsx
+++ src/app/tv/[id]/[season]/[episode]/player/page.tsx
@@ -2,81 +2,180 @@
 
 import { tmdb } from "@/api/tmdb";
 import { Params } from "@/types";
+import {
+  getTvShowLastPosition,
+} from "@/utils/localStorage";
 import { Spinner } from "@heroui/react";
 import { useQuery } from "@tanstack/react-query";
 import { notFound } from "next/navigation";
-import { use } from "react";
+import {
+  use,
+  useEffect,
+  useState,
+} from "react";
 import dynamic from "next/dynamic";
 import { NextPage } from "next";
-import { getTvShowLastPosition } from "@/actions/histories";
-const TvShowPlayer = dynamic(() => import("@/components/sections/TV/Player/Player"));
 
-const TvShowPlayerPage: NextPage<Params<{ id: number; season: number; episode: number }>> = ({
-  params,
-}) => {
-  const { id, season, episode } = use(params);
+const TvShowPlayer =
+  dynamic(
+    () =>
+      import(
+        "@/components/sections/TV/Player/Player"
+      ),
+  );
+
+const TvShowPlayerPage: NextPage<
+  Params<{
+    id: number;
+    season: number;
+    episode: number;
+  }>
+> = ({ params }) => {
+  const {
+    id,
+    season,
+    episode,
+  } = use(params);
 
   const {
     data: tv,
     isPending: isPendingTv,
     error: errorTv,
   } = useQuery({
-    queryFn: () => tmdb.tvShows.details(id),
-    queryKey: ["tv-show-player-details", id],
+    queryFn: () =>
+      tmdb.tvShows.details(id),
+
+    queryKey: [
+      "tv-show-player-details",
+      id,
+    ],
   });
 
   const {
     data: seasonDetail,
-    isPending: isPendingSeason,
+    isPending:
+      isPendingSeason,
     error: errorSeason,
   } = useQuery({
-    queryFn: () => tmdb.tvShows.season(id, season),
-    queryKey: ["tv-show-season", id, season],
+    queryFn: () =>
+      tmdb.tvShows.season(
+        id,
+        season,
+      ),
+
+    queryKey: [
+      "tv-show-season",
+      id,
+      season,
+    ],
   });
 
-  const { data: startAt, isPending: isPendingStartAt } = useQuery({
-    queryFn: () => getTvShowLastPosition(id, season, episode),
-    queryKey: ["tv-show-player-start-at", id, season, episode],
-  });
+  const [startAt, setStartAt] =
+    useState(0);
 
-  if (isPendingTv || isPendingSeason || isPendingStartAt) {
-    return <Spinner size="lg" className="absolute-center" color="warning" variant="simple" />;
+  useEffect(() => {
+    setStartAt(
+      getTvShowLastPosition(
+        id,
+        season,
+        episode,
+      ),
+    );
+  }, [
+    id,
+    season,
+    episode,
+  ]);
+
+  if (
+    isPendingTv ||
+    isPendingSeason
+  ) {
+    return (
+      <Spinner
+        size="lg"
+        className="absolute-center"
+        color="warning"
+        variant="simple"
+      />
+    );
   }
 
-  const EPISODE = seasonDetail?.episodes.find(
-    (e) => e.episode_number.toString() === episode.toString(),
-  );
+  if (
+    !seasonDetail ||
+    !tv ||
+    errorTv ||
+    errorSeason
+  ) {
+    return notFound();
+  }
 
-  if (!EPISODE || errorTv || errorSeason) notFound();
+  const EPISODE =
+    seasonDetail.episodes.find(
+      (entry) =>
+        entry.episode_number.toString() ===
+        episode.toString(),
+    );
 
-  const isNotReleased = new Date(EPISODE.air_date) > new Date();
+  if (!EPISODE) {
+    return notFound();
+  }
 
-  if (isNotReleased) notFound();
+  const isNotReleased =
+    new Date(
+      EPISODE.air_date,
+    ) > new Date();
 
-  const currentEpisodeIndex = seasonDetail.episodes.findIndex(
-    (e) => e.episode_number === EPISODE.episode_number,
-  );
+  if (isNotReleased) {
+    return notFound();
+  }
+
+  const currentEpisodeIndex =
+    seasonDetail.episodes.findIndex(
+      (entry) =>
+        entry.episode_number ===
+        EPISODE.episode_number,
+    );
 
   const nextEpisodeNumber =
-    currentEpisodeIndex < seasonDetail.episodes.length - 1
-      ? new Date(seasonDetail.episodes[currentEpisodeIndex + 1].air_date) > new Date()
+    currentEpisodeIndex <
+    seasonDetail.episodes.length - 1
+      ? new Date(
+          seasonDetail.episodes[
+            currentEpisodeIndex + 1
+          ].air_date,
+        ) > new Date()
         ? null
-        : seasonDetail.episodes[currentEpisodeIndex + 1].episode_number
+        : seasonDetail.episodes[
+            currentEpisodeIndex + 1
+          ].episode_number
       : null;
 
   const prevEpisodeNumber =
-    currentEpisodeIndex > 0 ? seasonDetail.episodes[currentEpisodeIndex - 1].episode_number : null;
+    currentEpisodeIndex > 0
+      ? seasonDetail.episodes[
+          currentEpisodeIndex - 1
+        ].episode_number
+      : null;
 
   return (
     <TvShowPlayer
       tv={tv}
       id={id}
       seriesName={tv.name}
-      seasonName={seasonDetail.name}
+      seasonName={
+        seasonDetail.name
+      }
       episode={EPISODE}
-      episodes={seasonDetail.episodes}
-      nextEpisodeNumber={nextEpisodeNumber}
-      prevEpisodeNumber={prevEpisodeNumber}
+      episodes={
+        seasonDetail.episodes
+      }
+      nextEpisodeNumber={
+        nextEpisodeNumber
+      }
+      prevEpisodeNumber={
+        prevEpisodeNumber
+      }
       startAt={startAt}
     />
   );
