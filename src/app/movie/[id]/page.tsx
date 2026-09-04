8fcd0d17ab03ed987b5d5af0974fe34e98a94ecf
Ray <d2arknotfound@gmail.com>
2026-09-04 23:41:24 +0530

    Refactor MovieDetailPage to MoviePlayerPage

--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -1,22 +1,19 @@
 "use client";
 
-import { Suspense, use } from "react";
-import { Spinner } from "@heroui/spinner";
-import { useQuery } from "@tanstack/react-query";
 import { tmdb } from "@/api/tmdb";
-import { Cast } from "tmdb-ts/dist/types/credits";
-import { notFound } from "next/navigation";
-import { Image } from "tmdb-ts";
-import dynamic from "next/dynamic";
+import MoviePlayer from "@/components/sections/Movie/Player/Player";
 import { Params } from "@/types";
+import { getMovieLastPosition } from "@/utils/localStorage";
+import { isEmpty } from "@/utils/helpers";
+import { Spinner } from "@heroui/react";
+import { useQuery } from "@tanstack/react-query";
 import { NextPage } from "next";
-const PhotosSection = dynamic(() => import("@/components/ui/other/PhotosSection"));
-const BackdropSection = dynamic(() => import("@/components/sections/Movie/Detail/Backdrop"));
-const OverviewSection = dynamic(() => import("@/components/sections/Movie/Detail/Overview"));
-const CastsSection = dynamic(() => import("@/components/sections/Movie/Detail/Casts"));
-const RelatedSection = dynamic(() => import("@/components/sections/Movie/Detail/Related"));
+import { notFound } from "next/navigation";
+import { use, useEffect, useState } from "react";
 
-const MovieDetailPage: NextPage<Params<{ id: number }>> = ({ params }) => {
+const MoviePlayerPage: NextPage<
+  Params<{ id: number }>
+> = ({ params }) => {
   const { id } = use(params);
 
   const {
@@ -25,38 +22,46 @@ const MovieDetailPage: NextPage<Params<{ id: number }>> = ({ params }) => {
     error,
   } = useQuery({
     queryFn: () =>
-      tmdb.movies.details(id, [
-        "images",
-        "videos",
-        "credits",
-        "keywords",
-        "recommendations",
-        "similar",
-        "reviews",
-        "watch/providers",
-      ]),
-    queryKey: ["movie-detail", id],
+      tmdb.movies.details(id),
+
+    queryKey: [
+      "movie-player-detail",
+      id,
+    ],
   });
 
+  const [startAt, setStartAt] =
+    useState(0);
+
+  useEffect(() => {
+    setStartAt(
+      getMovieLastPosition(id),
+    );
+  }, [id]);
+
   if (isPending) {
-    return <Spinner size="lg" className="absolute-center" variant="simple" />;
+    return (
+      <Spinner
+        size="lg"
+        className="absolute-center"
+        variant="simple"
+      />
+    );
   }
 
-  if (error) notFound();
+  if (
+    error ||
+    isEmpty(movie)
+  ) {
+    return notFound();
+  }
 
   return (
-    <div className="mx-auto max-w-5xl">
-      <Suspense fallback={<Spinner size="lg" className="absolute-center" variant="simple" />}>
-        <div className="flex flex-col gap-10">
-          <BackdropSection movie={movie} />
-          <OverviewSection movie={movie} />
-          <CastsSection casts={movie.credits.cast as Cast[]} />
-          <PhotosSection images={movie.images.backdrops as Image[]} />
-          <RelatedSection movie={movie} />
-        </div>
-      </Suspense>
-    </div>
+    <MoviePlayer
+      movie={movie}
+      startAt={startAt}
+    />
   );
 };
 
-export default MovieDetailPage;
+export default MoviePlayerPage;
