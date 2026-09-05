34749434ca0318d65d2493c6a089bc80d981deb1
Ray <d2arknotfound@gmail.com>
2026-09-05 23:55:37 +0530

    Refactor imports and format code in Player.tsx

--- src/components/sections/Movie/Player/Player.tsx
+++ src/components/sections/Movie/Player/Player.tsx
@@ -1,42 +1,60 @@
+"use client";
+
 import { SpacingClasses } from "@/utils/constants";
 import { siteConfig } from "@/config/site";
 import useBreakpoints from "@/hooks/useBreakpoints";
 import { cn } from "@/utils/helpers";
 import { mutateMovieTitle } from "@/utils/movies";
 import { getMoviePlayers } from "@/utils/players";
-import { useDisclosure, useDocumentTitle, useIdle } from "@mantine/hooks";
+import {
+  useDisclosure,
+  useDocumentTitle,
+  useIdle,
+} from "@mantine/hooks";
 import dynamic from "next/dynamic";
-import { parseAsInteger, useQueryState } from "nuqs";
+import {
+  parseAsInteger,
+  useQueryState,
+} from "nuqs";
+import {
+  useRef,
+} from "react";
 import { MovieDetails } from "tmdb-ts/dist/types/movies";
 import { usePlayerEvents } from "@/hooks/usePlayerEvents";
 import WatchPlayer from "@/components/WatchPlayer";
 
-const MoviePlayerHeader = dynamic(
-  () => import("./Header"),
-);
+const MoviePlayerHeader =
+  dynamic(
+    () => import("./Header"),
+  );
 
-const MoviePlayerSourceSelection = dynamic(
-  () => import("./SourceSelection"),
-);
+const MoviePlayerSourceSelection =
+  dynamic(
+    () => import("./SourceSelection"),
+  );
 
 interface MoviePlayerProps {
   movie: MovieDetails;
   startAt?: number;
 }
 
-const MoviePlayer: React.FC<MoviePlayerProps> = ({
+const MoviePlayer: React.FC<
+  MoviePlayerProps
+> = ({
   movie,
   startAt,
 }) => {
-  const players = getMoviePlayers(
-    movie.id,
-    startAt,
-  );
+  const players =
+    getMoviePlayers(
+      movie.id,
+      startAt,
+    );
 
   const title =
     mutateMovieTitle(movie);
 
-  const idle = useIdle(3000);
+  const idle =
+    useIdle(3000);
 
   const { mobile } =
     useBreakpoints();
@@ -52,28 +70,44 @@ const MoviePlayer: React.FC<MoviePlayerProps> = ({
     parseAsInteger.withDefault(0),
   );
 
-  usePlayerEvents({
-    saveHistory: true,
+  /*
+   * This ref always points at the iframe
+   * currently mounted by WatchPlayer.
+   */
+  const playerFrameRef =
+    useRef<HTMLIFrameElement | null>(
+      null,
+    );
 
-    metadata: {
-      mediaId: movie.id,
-      mediaType: "movie",
+  const {
+    getCurrentTime,
+    flushProgress,
+  } =
+    usePlayerEvents({
+      saveHistory: true,
 
-      title,
+      playerFrameRef,
 
-      backdrop_path:
-        movie.backdrop_path ?? "",
+      metadata: {
+        mediaId: movie.id,
+        mediaType: "movie",
 
-      poster_path:
-        movie.poster_path ?? undefined,
+        title,
 
-      release_date:
-        movie.release_date ?? "",
+        backdrop_path:
+          movie.backdrop_path ?? "",
 
-      vote_average:
-        movie.vote_average ?? 0,
-    },
-  });
+        poster_path:
+          movie.poster_path ??
+          undefined,
+
+        release_date:
+          movie.release_date ?? "",
+
+        vote_average:
+          movie.vote_average ?? 0,
+      },
+    });
 
   useDocumentTitle(
     `Play ${title} | ${siteConfig.name}`,
@@ -117,6 +151,15 @@ const MoviePlayer: React.FC<MoviePlayerProps> = ({
           }
           onServerChange={
             setSelectedSource
+          }
+          getCurrentTime={
+            getCurrentTime
+          }
+          flushProgress={
+            flushProgress
+          }
+          iframeRef={
+            playerFrameRef
           }
         />
       </div>
