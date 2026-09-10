a5a1b6367727a5189b8fce8176576525e44002f5
Ray <d2arknotfound@gmail.com>
2026-09-10 18:21:21 +0530

    Update embed source URLs for RyuFlix

--- src/utils/players.ts
+++ src/utils/players.ts
@@ -36,7 +36,7 @@ export const getMoviePlayers = (id: string | number, startAt?: number): PlayersP
     },
     {
       title: "<RyuFlix Embed>",
-      source: `https://www.2embed.cc/embed/${id}`,
+      source: `https://vidsrc.buzz/embed/movie/${id}`,
       ads: true,
     },
     {
@@ -139,7 +139,7 @@ export const getTvShowPlayers = (
     },
     {
       title: "<RyuFlix Embed>",
-      source: `https://2embed.cc/embed/tv/${id}/${season}/${episode}`,
+      source: `https://vidsrc.buzz/embed/tv/${id}/${season}/${episode}`,
       ads: true,
     },
     {
