714309a6668b1288f4755e7ff4afd7cc4f39c49b
Ray <d2arknotfound@gmail.com>
2026-09-11 23:30:42 +0530

    Integrate PopAds component and remove Script
    Replaced Script component with PopAds for ad integration.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ import RyuFlixCursor from "@/components/ui/RyuFlixCursor";
 import { SpacingClasses } from "@/utils/constants";
 import { NuqsAdapter } from "nuqs/adapters/next/app";
 import { Suspense } from "react";
-import Script from "next/script";
+import PopAds from "@/components/ads/PopAds";
 
 export const metadata: Metadata = {
   title: siteConfig.name,
@@ -80,26 +80,12 @@ export default function RootLayout({
       <body
         className={cn(
           "antialiased select-none",
-          Poppins.className
+          Poppins.className,
         )}
       >
         <RyuFlixCursor />
 
-        {/* ================================= */}
-        {/* POPADS ANTI-ADBLOCK GLOBAL         */}
-        {/* ================================= */}
-
-        <Script
-          id="popads-global"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              /*<![CDATA[/* */
-              (function(){var h=window,a="b94baacd0f42e920a031d6b4501ecd21",o=[["siteId",710+199-699-599-834+5319580],["minBid",0],["popundersPerIP","0"],["delayBetween",0],["default",false],["defaultPerDay",0],["topmostLayer","auto"]],b=["d3d3LnByZW1pdW12ZXJ0aXNpbmcuY29tL0N5L3VrblZpcC9mYm9vdHN0cmFwLWRhdGV0aW1lcGlja2VyLm1pbi5qcw==","ZDJqMDQyY2oxNDIxd2kuY2xvdWRmcm9udC5uZXQvcWpxdWVyeS5QcmludEFyZWEubWluLmpz"],d=-1,y,z,g=function(){clearTimeout(z);d++;if(b[d]&&!(1814686616000<(new Date).getTime()&&1<d)){y=h.document.createElement("script");y.type="text/javascript";y.async=!0;var i=h.document.getElementsByTagName("script")[0];y.src="https://"+atob(b[d]);y.crossOrigin="anonymous";y.onerror=g;y.onload=function(){clearTimeout(z);h[a.slice(0,16)+a.slice(0,16)]||g()};z=setTimeout(g,5E3);i.parentNode.insertBefore(y,i)}};if(!h[a]){try{Object.freeze(h[a]=o)}catch(e){}g()}})();
-              /*]]>/* */
-            `,
-          }}
-        />
+        <PopAds />
 
         <Suspense>
           <NuqsAdapter>
@@ -115,7 +101,7 @@ export default function RootLayout({
                   <main
                     className={cn(
                       "relative z-10 container mx-auto max-w-full",
-                      SpacingClasses.main
+                      SpacingClasses.main,
                     )}
                   >
                     {children}
@@ -134,4 +120,4 @@ export default function RootLayout({
       </body>
     </html>
   );
-                  }
+}
